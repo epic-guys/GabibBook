8b46f391c26480060676cc7fdea172bcf848877f
WAPEETY <wapeety@protonmail.com>
2024-06-14 13:48:15 +0200

    fixed login

--- api/src/routes/auth.route.ts
+++ api/src/routes/auth.route.ts
@@ -1,6 +1,6 @@
 import {Request, Response, Router} from 'express';
 import { UserType } from '../models/user.model';
-import { register } from '../controllers/auth.controller';
+import { register, login } from '../controllers/auth.controller';
 import argon2 from 'argon2';
 import passport from 'passport';
 import jwt  from 'jsonwebtoken';
@@ -13,6 +13,13 @@ const authRouter = Router();
 
 authRouter.post('/register',validateUser, register);
 
-authRouter.get('/login', passport.authenticate('basic', {session: false}));
+authRouter.get('/login', passport.authenticate('basic', {session: false}), async (req: Request, res: Response) => {
+    try{
+        return login(req, res);
+    }
+    catch(e){
+        res.status(500).json({message: 'Internal Server Error'});
+    }
+});
 
 export default authRouter;
