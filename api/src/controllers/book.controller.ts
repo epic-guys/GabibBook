0e84997e82bbf422da67fbd5687c59d03e0126e1
WAPEETY <wapeety@protonmail.com>
2024-06-18 20:13:09 +0200

    fix banned book pt. 2

--- api/src/controllers/book.controller.ts
+++ api/src/controllers/book.controller.ts
@@ -145,7 +145,7 @@ export async function deleteBook(req: Request, res: Response) {
      let book = await Book.findById(req.params.id).exec();
      if (!book) return res.status(404).json({ message: 'Book not found' });
      else {
-          book.deleteOne();
+          book.banned = true;
           await book.save();
           return res.status(200).json({ message: 'Book deleted' });
      }
