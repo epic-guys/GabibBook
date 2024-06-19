f79b335facaea0bba8bec212461ff75da0006730
Simon Scoltock <simon.scoltock.work@gmail.com>
2024-06-20 00:29:50 +0200

    added controls for "closed_date" on books

--- api/src/controllers/book.controller.ts
+++ api/src/controllers/book.controller.ts
@@ -105,7 +105,8 @@ export async function createBook(req: Request, res: Response) {
 }
 
 export async function createOffer(req:Request<{id: string}, any, {value: number}>, res: Response) {
-    let book = await Book.findById(req.params.id).where({banned: false}).exec();
+    let now = new Date();
+    let book = await Book.findById(req.params.id).where({banned: false, closed_date: { $gt: now }}).exec();
     let user = req.user as UserType;
     if (book == null) {
         res.status(404).send({message: 'Book not found'})
@@ -136,7 +137,8 @@ export async function createOffer(req:Request<{id: string}, any, {value: number}
 }
 
 export async function updateBook(req: Request, res: Response) {
-     let book = await Book.findById(req.params.id).where({banned: false}).exec();
+     let now = new Date();
+     let book = await Book.findById(req.params.id).where({banned: false, closed_date: { $gt: now }}).exec();
      if (!book) return res.status(404).json({ message: 'Book not found' });
      else {
           book.set(req.body);
