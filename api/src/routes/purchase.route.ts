10896aea96bdabdcafa957b46a0283af4ce470e8
WAPEETY <wapeety@protonmail.com>
2024-06-19 19:02:22 +0200

    implemented check for user in purchases

--- api/src/routes/purchase.route.ts
+++ api/src/routes/purchase.route.ts
@@ -4,15 +4,20 @@ import passport from 'passport';
 import config from '../config';
 import { isSameUser } from '../middleware/user.middleware';
 import { getPurchasesByBuyerId, getPurchasesBySellerId, changePurchaseStatus } from '../controllers/purchase.controller';
+import { UserType } from '../models/user.model';
 
 
 const purchaseRouter = Router();
 
-//TODO: check if the user is the same as the one being requested
 purchaseRouter.get('/', passport.authenticate('jwt', config.passportOptions), (req: Request, res: Response) => {
     try {
-        if (req.query.buyer_id) return getPurchasesByBuyerId(req, res);
-        else if (req.query.seller_id) return getPurchasesBySellerId(req, res);
+        const user = req.user as UserType;
+        if (req.query.buyer_id && req.query.buyer_id === user._id!.toString()) {
+            return getPurchasesByBuyerId(req, res);
+        }
+        else if (req.query.seller_id && req.query.seller_id === user._id!.toString()) {
+            return getPurchasesBySellerId(req, res);
+        }
         else res.status(404).json({ message: 'Bad Request' });
     } catch (err: any) {
         res.status(500).json({ message: 'Internal Server Error' });
