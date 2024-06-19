d07c921488af9f72faa12ebb1aca9f66ab5c0caf
WAPEETY <wapeety@protonmail.com>
2024-06-20 01:47:50 +0200

    commented socket since it's not finished

--- api/src/socket.ts
+++ api/src/socket.ts
@@ -1,9 +1,9 @@
 import { Server, Socket } from 'socket.io';
 import {BookSocket} from './sockets/book.socket';
-import {ChatSocket} from './sockets/chat.socket';
+//import {ChatSocket} from './sockets/chat.socket';
 
 
 export const io = new Server();
 
 export const bookSocket = new BookSocket(io);
-export const chatSocket = new ChatSocket(io);
+//export const chatSocket = new ChatSocket(io);
