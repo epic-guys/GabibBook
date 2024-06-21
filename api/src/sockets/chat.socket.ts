b9f245e00bcb27051955aa7ee1e766cb3f280314
Alvise Favero <66018479+alvi-se@users.noreply.github.com>
2024-06-22 00:11:23 +0200

    Rewrite of chat socket

--- api/src/sockets/chat.socket.ts
+++ api/src/sockets/chat.socket.ts
@@ -1,27 +1,30 @@
-import { Namespace, Server, Socket } from "socket.io";
-import { JwtPayload, verify } from "jsonwebtoken";
+import { DisconnectReason, Namespace, Server, Socket } from "socket.io";
+import { verify } from "jsonwebtoken";
 import config from "../config";
 import { User, UserType } from "../models/user.model";
 import { Book, BookType } from "../models/book.model";
 import { Chat, ChatType, Message, PopulatedChatType } from "../models/chat.model";
-import { Request, Response } from "express";
-import passport from "passport";
 import { HydratedDocument } from "mongoose";
+import logger from "../logger";
 
+
+type ChatSocketType = Socket<any, any, any, {user?: UserType, book: BookType}>;
 
 export class ChatSocket {
     io: Server;
     chatIO: Namespace;
+    bookSockets: {[bookId: string]: Set<ChatSocketType>} = {};
 
     constructor(io: Server) {
         this.io = io;
         this.chatIO = this.io.of('/chats');
 
+        this.chatIO.on('connection', async (socket: ChatSocketType) => {
+            logger.log('info', 'New connection to chat socket');
 
-        this.chatIO.on('connection', async (socket: Socket<any, any, any, {user?: UserType}>) => {
-            let openChats: {[chatId: string]: HydratedDocument<PopulatedChatType>} = {}
-
+            // Authentication
             socket.on('auth', async (event: any) => {
+                logger.info('Authenticating socket ' + socket.id);
                 try {
                     let jwt: string = event?.jwt;
                     if (!jwt) {
@@ -42,35 +45,86 @@ export class ChatSocket {
 
             });
 
-            socket.on('subscribeChat', async (event: any) => {
+            socket.on('subscribeBook', async (event: any) => {
+                logger.info('Subscribing to book ' + event.bookId);
                 try {
                     if (!socket.data.user) {
                         throw new Error('Not authenticated');
                     }
-                    let chat = await Chat.
-                        findById(event.chatId).
-                        populate<Pick<PopulatedChatType, 'book' | 'buyer'>>('book').exec();
-                    if (!chat) {
-                        throw new Error('Chat not found');
+                    let book = await Book.findById(event.bookId).exec();
+                    if (!book) {
+                        throw new Error('Book not found');
                     }
 
-                    // If 'buyer' is not set, then the chat is public
-                    if (!chat.buyer ||
-                        // If 'buyer' is set, then only the buyer and the owner can access the chat
-                        chat.buyer._id === socket.data.user!._id! ||
-                        chat.book.owner === socket.data.user!._id!
-                   ) {
-                        socket.join(this.chatRoom(chat));
-                        openChats[chat!._id.toString()] = chat;
-                        // receiveMessage is from the perspective of the client, it's the client that receives the messages
-                        this.newMessages(chat);
+                    let chats: ChatType[];
+
+                    // The user is the owner of the book
+                    if (book.owner.toString() === socket.data.user!._id!.toString()) {
+                        chats = await Chat.find({book: event.bookId}).exec();
+                        // If there are no chats, create only the public chat
+                        if (chats.length === 0) {
+                            let chat = new Chat({
+                                book: book._id,
+                                buyer: null,
+                                messages: []
+                            });
+                            await chat.save();
+                            chats.push(chat);
+                        }
+
                     }
+                    // The user is a buyer, we need only the public chat and the chat with the buyer
                     else {
-                        throw new Error('Forbidden');
+                        let publicChat = await Chat.findOne({book: event.bookId, buyer: null}).exec();
+                        let buyerChat = await Chat.findOne({book: event.bookId, buyer: socket.data.user!._id!}).exec();
+                        if (!publicChat) {
+                            publicChat = new Chat({
+                                book: book._id,
+                                buyer: null,
+                                messages: []
+                            });
+                            await publicChat.save();
+                        }
+                        if (!buyerChat) {
+                            buyerChat = new Chat({
+                                book: book._id,
+                                buyer: socket.data.user!._id!,
+                                messages: []
+                            });
+                            await buyerChat.save();
+                        }
+
+                        chats = [publicChat, buyerChat];
                     }
+
+                    // If the user is the first to subscribe to the book, create the set
+                    if (this.bookSockets[event.bookId] === undefined) {
+                        this.bookSockets[event.bookId] = new Set();
+                    }
+
+                    for (let chat of chats) {
+                        let event = {
+                            _id: chat._id,
+                            book: chat.book,
+                            buyer: chat.buyer,
+                            messages: chat.messages
+                        }
+                        socket.emit('message', event);
+                    }
+
+                    // I put this here to ensure that any other message is sent after the whole chat
+                    this.bookSockets[event.bookId].add(socket);
+                    socket.data.book = book;
 
                 } catch (e: any) {
                     socket.emit('error', {'message': e.message});
+                    logger.error(e.message);
+                }
+            });
+            
+            socket.on('disconnect', (reason: DisconnectReason) => {
+                if (socket.data.book) {
+                    this.bookSockets[socket.data.book._id!.toString()].delete(socket);
                 }
             });
 
@@ -80,45 +134,59 @@ export class ChatSocket {
                         throw new Error('Not authenticated');
                     }
 
-                    if (!(event!.chatId in openChats)) {
-                        throw new Error('Chat not subscribed');
+                    let chat = await Chat.findById(event.chatId).populate<Pick<PopulatedChatType, 'book' | 'buyer'>>('book buyer').exec();
+
+                    if (!chat) {
+                        throw new Error('Chat not found');
                     }
 
-                    let chat = openChats[event.chatId];
+                    if (!this.canAccessChat(chat, socket.data.user)) {
+                        throw new Error('Forbidden');
+                    }
+                    
                     let message: Message = {
                         sender: socket.data.user._id!,
-                        text: event!.text,
+                        text: event.text,
                         date: new Date()
-                    }
+                    };
+
                     chat.messages.push(message);
                     await chat.save();
-                    this.newMessages(chat, [message]);
+
+                    this.sendMessages(chat, [message]);
+
                 } catch (e: any) {
                     socket.emit('error', {'message': e.message});
                 }
-            })
+            });
         });
     }
 
-    /**
-     * Broadcasts the new messages to subscribed users.
-     * If no messages parameter is specified, all the old messages are sent.
-     * Instead, if it is specified, it sends only these messages.
-     */
-    public newMessages(chat: PopulatedChatType, messages?: Message[]) {
+    public sendMessages(chat: PopulatedChatType, messages: Message[], socket?: ChatSocketType) {
         let event = {
             _id: chat._id,
-            messages: (messages ?? chat.messages)
-        };
+            book: chat.book._id,
+            buyer: chat.buyer?._id ?? null,
+            messages: messages
+        }
 
-        this.chatIO.to(this.chatRoom(chat)).emit('message', event);
+        if (socket) {
+            socket.emit('message', event);
+        }
+        else {
+            this.bookSockets[chat.book._id!.toString()].forEach(socket => {
+                socket.emit('message', event);
+            });
+        }
     }
 
-    private chatRoom(chat: PopulatedChatType): string {
-        // Chat rooms have the following format:
-        // chat:<book_id>:[<buyer_id>]
-        // where buyer_id is set only if the chat is private
-        return 'chat:' + chat.book._id!.toString() + ':' + (chat.buyer?._id!.toString() ?? '');
+    public canAccessChat(chat: PopulatedChatType, user: UserType): boolean {
+        // Is the chat public?
+        return !chat.buyer
+            // Is the user the buyer?
+            || chat.buyer._id!.toString() === user._id?.toString()
+            // Is the user the owner of the book?
+            || chat.book.owner.toString() === user._id?.toString();
     }
 }
 
