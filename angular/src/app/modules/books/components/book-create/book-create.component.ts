f5cfbe63b97c6fa1eb7c996f01ecbc3c27a5f766
WAPEETY <wapeety@protonmail.com>
2024-06-22 09:53:11 +0200

    finished book create

--- angular/src/app/modules/books/components/book-create/book-create.component.ts
+++ angular/src/app/modules/books/components/book-create/book-create.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { BookService } from 'src/app/common/services/books/book.service';
 
 @Component({
   selector: 'app-book-create',
@@ -14,22 +17,104 @@ export class BookCreateComponent {
     isbn : new FormControl('', [Validators.required]),
     author: new FormControl('', [Validators.required]),
     start_price: new FormControl('', [Validators.required]),
+    reserve_price: new FormControl('', [Validators.required]),
     degree_course: new FormControl('', [Validators.required]),
     close_date: new FormControl('', [Validators.required]),
     open_date: new FormControl('', [Validators.required]),
   });
 
-  constructor() { }
+  constructor(private bookService: BookService, private snackBar: MatSnackBar, private router: Router) { }
 
-  onSubmit() {
-    //TODO: check if form is valid
-    //TODO: send form data to backend
+  async onSubmit() {
+    if(this.form.invalid) {
+      return;
+    }
+
+    if(this.form.value.start_price >= this.form.value.reserve_price) {
+      this.snackBar.open('Start price must be lower than reserve price', 'Close', {
+        duration: 2000,
+      });
+      return;
+    }
+
+    if(this.form.value.open_date >= this.form.value.close_date) {
+      this.snackBar.open('Open date must be lower than close date', 'Close', {
+        duration: 2000,
+      });
+      return;
+    }
+
+    if(this.form.value.open_date < new Date().toISOString()) {
+      this.snackBar.open('Open date cannot be in the past', 'Close', {
+        duration: 2000,
+      });
+      return;
+    }
+
+    let cover_base64 = null;
+
+    if(this.coverfile) {
+      cover_base64 = await this.fileToBase64(this.coverfile); 
+    }
+
+    const data = {
+      title: this.form.value.title,
+      isbn: this.form.value.isbn,
+      author: this.form.value.author,
+      degree_course: this.form.value.degree_course,
+      close_date: this.form.value.close_date,
+      open_date: this.form.value.open_date,
+      start_price: this.form.value.start_price,
+      reserve_price: this.form.value.reserve_price,
+      cover: cover_base64 ? cover_base64 : '',
+    }
+
+    const observer = {
+      next: (response: any) => {
+        console.log('response', response);
+        this.snackBar.open('Book created', 'Close', {
+          duration: 2000,
+        });
+        this.router.navigate(['/books/' + response._id]);
+      },
+      error: (error: any) => {
+        console.log('error', error);
+        this.snackBar.open('Error creating book', 'Close', {
+          duration: 2000,
+        });
+      }
+    }
+
+    this.bookService.createBook(data).subscribe(observer);
+  }
+
+  fileToBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        resolve(reader.result as string);
+      };
+      reader.onerror = (error) => {
+        console.error('Error: ', error);
+        reject(error);
+      };
+    });
   }
 
   onFileChange(event: any) {
-    console.log('file change');
-    console.log(event);
-    //TODO: check if file is an image if not reset the input
+    if(event.target.files.length === 0) {
+      return;
+    }
+
+    if(event.target.files[0].type.split('/')[0] !== 'image') {
+      console.log('not an image');
+      this.snackBar.open('Please select an image', 'Close', {
+        duration: 2000,
+      });
+      event.target.value = '';
+      return;
+    }
     this.coverfile = event.target.files[0];
   }
 }
